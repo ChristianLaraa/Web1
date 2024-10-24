cc3f360a26e76cde01e721233af1cf422ba43e48
Christian Lara <christian.lara.091204@gmail.com>
2024-10-23 23:19:29 -0600

    add full

--- 171024/conexion.php
+++ 171024/conexion.php
@@ -1,14 +1,14 @@
 <?php
-$host_Db = "localhost"; // Dirección del servidor
-$user_name = "root"; // Usuario por defecto de MySQL
-$user_pass= "Lunapug09."; // La contraseña es vacía por defecto en XAMPP
-$db_name = "fes_aragon"; // Nombre de tu base de datos
+$host_db = "localhost"; 
+$user_name = "root"; 
+$user_pass = "Lunapug09."; 
+$db_name = "fes_aragon"; 
 
-$conexion = new mysqli($host_db,$user_name,$user_pass,$db_name);
+$conexion = new mysqli($host_db, $user_name, $user_pass, $db_name);
 
-if($conexion->connect_error){
-}
-else{
+if ($conexion->connect_error) {
+    die("Error de conexión: " . $conexion->connect_error);
+} else {
     echo "Conexión realizada";
 }
 ?>
